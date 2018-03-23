9dd9e3df11538dddd98bd432fbbfa80ead76d898
Adrian <adrian971029@gmail.com>
2018-03-23 10:08:29 -0300

    Projecto creado

---
+++ app/src/main/java/com/adrian971029/filmesfamosos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.adrian971029.filmesfamosos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
