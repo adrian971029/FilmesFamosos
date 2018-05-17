f5a42ba8859a9d6ead69f8a51f1f444115ced271
Adrian <adrian971029@gmail.com>
2018-05-17 11:58:52 -0300

    Add VideoHttp que se encarga de la conexion al endpoint da api

--- app/src/main/java/com/adrian971029/filmesfamosos/utils/Constants.java
+++ app/src/main/java/com/adrian971029/filmesfamosos/utils/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
     public static final String URL_BASE = "https://api.themoviedb.org/3/";
     public static final String POPULAR_MOVIE_URL = "movie/popular?api_key=";
     public static final String TOP_RATED_URL = "movie/top_rated?api_key=";
+    public static final String VIDEO_URL = "movie/videos?api_key=";
     public static final String LANGUAGE = "pt-BR";
     public static final String Q_LANGUAGE = "&language=";
     public static final String GET_IMAGES = "http://image.tmdb.org/t/p/w185/";
---
+++ app/src/main/java/com/adrian971029/filmesfamosos/utils/network/VideoHttp.java
@@ -0,0 +1,80 @@
+package com.adrian971029.filmesfamosos.utils.network;
+
+import com.adrian971029.filmesfamosos.model.Video;
+import com.adrian971029.filmesfamosos.utils.Constants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoHttp {
+
+    public static List<Video> carregarVideoJson(){
+        try {
+            HttpURLConnection conexao = HttpConnection.connection(Constants.URL_BASE +
+                    Constants.VIDEO_URL +
+                    Constants.API_KEY +
+                    Constants.Q_LANGUAGE +
+                    Constants.LANGUAGE
+            );
+
+            int resposta = conexao.getResponseCode();
+            if(resposta == HttpURLConnection.HTTP_OK){
+                InputStream is = conexao.getInputStream();
+                JSONObject jsonObject = new JSONObject(HttpConnection.byteParaString(is));
+                return lerJsonMovies(jsonObject);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<Video> lerJsonMovies(JSONObject json) throws JSONException {
+        List<Video> listVideo = new ArrayList<Video>();
+        String id, iso_639_1, iso_3166_1, key, name, site, type;
+        Integer size;
+
+
+        JSONArray jsonResults = json.getJSONArray("results");
+        for (int i = 0; i < jsonResults.length(); i++){
+
+            JSONObject jsonId = jsonResults.getJSONObject(i);
+            JSONObject jsonIso_639_1 = jsonResults.getJSONObject(i);
+            JSONObject jsonIso_3166_1 = jsonResults.getJSONObject(i);
+            JSONObject jsonKey = jsonResults.getJSONObject(i);
+            JSONObject jsonName = jsonResults.getJSONObject(i);
+            JSONObject jsonSite = jsonResults.getJSONObject(i);
+            JSONObject jsonSize = jsonResults.getJSONObject(i);
+            JSONObject jsonType = jsonResults.getJSONObject(i);
+
+            id = jsonId.getString("id");
+            iso_639_1 = jsonIso_639_1.getString("iso_639_1");
+            iso_3166_1 = jsonIso_3166_1.getString("iso_3166_1");
+            key = jsonKey.getString("key");
+            name = jsonName.getString("name");
+            site = jsonSite.getString("site");
+            size = jsonSize.getInt("size");
+            type= jsonType.getString("type");
+
+            Video video = new Video(
+                    id,
+                    iso_639_1,
+                    iso_3166_1,
+                    key,
+                    name,
+                    site,
+                    size,
+                    type
+            )
+            listVideo.add(video);
+        }
+        return listVideo;
+    }
+
+}
